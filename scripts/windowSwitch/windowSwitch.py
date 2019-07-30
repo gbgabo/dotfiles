d5b4b49b4be47add0b6d1efeb6b5d197fce1a98b
gabo <gabriel.gaboardi13@gmail.com>
2019-07-30 20:04:41 -0300

    CLI support to list and focus window. Added a dedicated script only for dmenu support

--- scripts/windowSwitch/windowSwitch.py
+++ scripts/windowSwitch/windowSwitch.py
@@ -17,22 +17,31 @@ def get_windows(global_flag):
         for output in tree:
                 if(output.type == "workspace"):
                         if(global_flag or output.name == focused.workspace().name):
-                                print(output.name)
                                 for con in output:
                                         if(con.name is not None):
+                                                print(con.name)
                                                 cons.append(repr(con.name))
                                                 ids.append(con.id)
         return cons, ids
 
+def focus_window(window_name):
+        # Get id of the choosen window
+        conId = ids[cons.index(window_name)]
+
+        focus_cmd = "i3-msg '[con_id='{0}'] focus'".format(conId)
+        subprocess.Popen(focus_cmd, shell = True)
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-w", "--workspace", help="list opened windows only in the current workspace", action="store_true")
+parser.add_argument("-f", "--focus", help="focus a window by its name", action="store", type=str, metavar="window_name")
 args = parser.parse_args()
 if args.workspace:
-        dmenu_prompt = focused.workspace().name
         cons, ids = get_windows(False)
 else:
-        dmenu_prompt = "Window"
         cons, ids = get_windows(True)
+
+if args.focus:
+        focus_window(repr(args.focus))
 
 if(cons == []):
         # If there is no opened window
@@ -40,15 +49,3 @@ if(cons == []):
 
 # Format every container name as "choices" input for dmenu
 choices = repr("\n").join(cons)
-
-# Execute dmenu command and get its output
-dmenu_cmd = "echo -e {0} | dmenu -i -fn 'undefined' -p {1}:".format(choices, dmenu_prompt)
-process = subprocess.Popen(dmenu_cmd, shell = True, stdout=subprocess.PIPE)
-out, err = process.communicate(timeout=60)
-out = repr(out.decode("UTF-8").rstrip())
-
-# Get id of the choosen window
-conId = ids[cons.index(out)]
-
-focus_cmd = "i3-msg '[con_id='{0}'] focus'".format(conId)
-process = subprocess.Popen(focus_cmd, shell = True)
